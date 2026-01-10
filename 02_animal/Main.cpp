336b482efca5756b2c168a83b090b9680b6ca877
AthiwatJantasri <athiwat242515@gmail.com>
2026-01-10 22:06:52 +0700

    Update Main.cpp

--- 02_animal/Main.cpp
+++ 02_animal/Main.cpp
@@ -2,155 +2,125 @@
 #include <string>
 using namespace std;
 
-// --- 1. Enums ---
-enum Color { BLACK, WHITE, GREY, BROWN, DARK_BROWN, BLACK_STRIPES, WHITE_STRIPES };
-enum Size { SMALL, MEDIUM, LARGE, HUGE };
+enum class Color { BLACK, WHITE, GREY, BROWN };
+enum class Size { SMALL, MEDIUM, LARGE, HUGE };
 
-// Helper function to convert Enum to String for printing
-string colorToString(Color c) {
-    switch(c) {
-        case BLACK: return "BLACK";
-        case WHITE: return "WHITE";
-        case GREY: return "GREY";
-        case BROWN: return "BROWN";
-        case DARK_BROWN: return "DARK_BROWN";
-        case BLACK_STRIPES: return "BLACK_STRIPES";
-        case WHITE_STRIPES: return "WHITE_STRIPES";
-        default: return "UNKNOWN";
-    }
-}
-string sizeToString(Size s) {
-    switch(s) {
-        case SMALL: return "SMALL";
-        case MEDIUM: return "MEDIUM";
-        case LARGE: return "LARGE";
-        case HUGE: return "HUGE";
-        default: return "UNKNOWN";
-    }
-}
-
-// --- 2. Abstract Class: Animal ---
 class Animal {
 protected:
     Color color;
     Size size;
+
 public:
     Animal(Color c, Size s) : color(c), size(s) {}
-    virtual void sound() = 0; // Pure virtual function
-    virtual void eat() = 0;   // Pure virtual function
-    
-    void printInfo() {
-        // C++ ไม่มี reflection ชื่อคลาสแบบง่ายๆ ขอ Hardcode ชื่อในลูกแทน หรือใช้วิธี typeid แต่มันจะยาว
-        // ในที่นี้ขอปริ้นแค่ Attributes
-        cout << "Color: " << colorToString(color) << ", Size: " << sizeToString(size) << endl;
+    virtual void sound() = 0;
+    virtual void eat() = 0;
+
+    void info() {
+        cout << "__Animal__" << endl;
     }
 };
 
-// --- 3. Intermediate Classes ---
+// ======================
+
 class Mammalia : public Animal {
 protected:
     int numberBabies;
+
 public:
-    Mammalia(Color c, Size s, int babies) : Animal(c, s), numberBabies(babies) {}
-    void run() { cout << "Moving: Running on the ground." << endl; }
+    Mammalia(Color c, Size s, int n) : Animal(c, s), numberBabies(n) {}
+    void run() { cout << "Moving: Running on the ground\n"; }
 };
 
 class Aves : public Animal {
 public:
     Aves(Color c, Size s) : Animal(c, s) {}
-    void fly() { cout << "Moving: Flying in the sky." << endl; }
+    void fly() { cout << "Moving: Flying in the sky\n"; }
 };
 
 class Osteichthyes : public Animal {
 public:
     Osteichthyes(Color c, Size s) : Animal(c, s) {}
-    void swimming() { cout << "Moving: Swimming in the water." << endl; }
+    void swim() { cout << "Moving: Swimming in the sea\n"; }
 };
 
-// --- 4. Concrete Classes ---
+// ======================
+
 class Dog : public Mammalia {
 protected:
     bool fierce;
+
 public:
-    Dog(Color c, Size s, int babies, bool f) : Mammalia(c, s, babies), fierce(f) {}
-    void sound() override { cout << "Sound: Woof Woof!" << endl; }
-    void eat() override { cout << "Eat: Dog food and bones." << endl; }
-    void bark() { cout << "Action: Barking loudly!" << endl; }
+    Dog(Color c, Size s, int n, bool f) : Mammalia(c, s, n), fierce(f) {}
+
+    void sound() override { cout << "Sound: Wolf wolf\n"; }
+    void eat() override { cout << "Food: Bones\n"; }
+    void bark() { cout << "Action: Barking loud!\n"; }
     void bite() {
-        cout << "Fierce Status: " << (fierce ? "true" : "false") << endl;
-        cout << "Action: Warning! It bites!" << endl;
+        cout << "Fierce Status: " << fierce << endl;
+        cout << "It gonna bite you\n";
     }
 };
 
 class Bird : public Aves {
 protected:
-    string eggDesc;
+    string egg;
+
 public:
-    Bird(Color c, Size s, string egg) : Aves(c, s), eggDesc(egg) {}
-    void sound() override { cout << "Sound: Chirp Chirp!" << endl; }
-    void eat() override { cout << "Eat: Worms and seeds." << endl; }
+    Bird(Color c, Size s, string e) : Aves(c, s), egg(e) {}
+    void sound() override { cout << "Sound: Chip chip\n"; }
+    void eat() override { cout << "Food: Worms\n"; }
 };
 
 class Fish : public Osteichthyes {
 protected:
     string waterGroup;
+
 public:
-    Fish(Color c, Size s, string water) : Osteichthyes(c, s), waterGroup(water) {}
-    void sound() override { cout << "Sound: ... (Glub Glub)" << endl; }
-    void eat() override { cout << "Eat: Plankton and small fish." << endl; }
+    Fish(Color c, Size s, string w) : Osteichthyes(c, s), waterGroup(w) {}
+    void sound() override { cout << "Sound: Pop pop\n"; }
+    void eat() override { cout << "Food: Small Fish\n"; }
 };
 
-// --- 5. Specific Species ---
+// ======================
+
 class ThaiRidgeBack : public Dog {
-private:
     string origin;
+
 public:
-    ThaiRidgeBack(Color c, Size s, int babies, bool f, string o) : Dog(c, s, babies, f), origin(o) {}
-    void printHeader() { cout << "--- ThaiRidgeBack ---" << endl; } // Helper for header
+    ThaiRidgeBack(Color c, Size s, int n, bool f, string o)
+        : Dog(c, s, n, f), origin(o) {}
+
     void showOrigin() { cout << "Origin: " << origin << endl; }
 };
 
 class HummingBird : public Bird {
-private:
-    string location;
 public:
-    HummingBird(Color c, Size s, string egg, string loc) : Bird(c, s, egg), location(loc) {}
-    void printHeader() { cout << "--- HummingBird ---" << endl; }
+    HummingBird(Color c, Size s, string e) : Bird(c, s, e) {}
+};
+
+class AngleFish : public Fish {
+    string location;
+
+public:
+    AngleFish(Color c, Size s, string w, string l)
+        : Fish(c, s, w), location(l) {}
+
     void showLocation() { cout << "Location: " << location << endl; }
 };
 
-class AngelFish : public Fish {
-public:
-    AngelFish(Color c, Size s, string water) : Fish(c, s, water) {}
-    void printHeader() { cout << "--- AngelFish ---" << endl; }
-};
+// ======================
 
-// --- 6. Main ---
 int main() {
-    ThaiRidgeBack trb(BROWN, MEDIUM, 4, true, "Thailand");
-    trb.printHeader();
-    trb.printInfo();
-    trb.eat();
-    trb.run();
-    trb.sound();
-    trb.bark();
-    trb.bite();
-    trb.showOrigin();
-    cout << "-------------------------------" << endl;
+    ThaiRidgeBack dog(Color::BROWN, Size::MEDIUM, 5, true, "Thailand");
+    dog.sound(); dog.eat(); dog.run(); dog.bark(); dog.bite(); dog.showOrigin();
 
-    HummingBird hmb(GREY, SMALL, "Tiny White Egg", "Tropical Forest");
-    hmb.printHeader();
-    hmb.printInfo();
-    hmb.fly();
-    hmb.eat();
-    hmb.showLocation();
-    cout << "-------------------------------" << endl;
+    cout << "----------------\n";
 
-    AngelFish af(WHITE_STRIPES, SMALL, "Saltwater");
-    af.printHeader();
-    af.printInfo();
-    af.swimming();
-    af.eat();
+    HummingBird bird(Color::WHITE, Size::SMALL, "Small egg");
+    bird.fly(); bird.sound(); bird.eat();
 
-    return 0;
+    cout << "----------------\n";
+
+    AngleFish fish(Color::GREY, Size::SMALL, "Salt Water", "Pacific Ocean");
+    fish.swim(); fish.sound(); fish.eat(); fish.showLocation();
 }
