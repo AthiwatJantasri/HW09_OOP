6128c7e1578c32fc074329c1aa5098b3212984be
AthiwatJantasri <athiwat242515@gmail.com>
2026-01-10 22:06:24 +0700

    Update HelloWorld.cs

--- 02_animal/HelloWorld.cs
+++ 02_animal/HelloWorld.cs
@@ -1,20 +1,15 @@
 using System;
 
-// --- 1. Enums ---
-enum Color {
-    BLACK, WHITE, GREY, BROWN, DARK_BROWN, BLACK_STRIPES, WHITE_STRIPES
-}
+enum Color { BLACK, WHITE, GREY, BROWN, DARK_BROWN, BLACK_STRIPES, WHITE_STRIPES }
+enum Size { SMALL, MEDIUM, LARGE, HUGE }
 
-enum Size {
-    SMALL, MEDIUM, LARGE, HUGE
-}
-
-// --- 2. Abstract Class: Animal ---
-abstract class Animal {
+abstract class Animal
+{
     protected Color color;
     protected Size size;
 
-    public Animal(Color color, Size size) {
+    public Animal(Color color, Size size)
+    {
         this.color = color;
         this.size = size;
     }
@@ -22,155 +17,150 @@ abstract class Animal {
     public abstract void Sound();
     public abstract void Eat();
 
-    public void PrintInfo() {
-        Console.WriteLine("--- " + this.GetType().Name + " ---");
-        Console.WriteLine("Color: " + color + ", Size: " + size);
+    public void Info()
+    {
+        Console.WriteLine($"__{GetType().Name}__");
+        Console.WriteLine($"Color: {color} Size: {size}");
     }
 }
 
-// --- 3. Intermediate Classes ---
-abstract class Mammalia : Animal {
+// ======================
+
+abstract class Mammalia : Animal
+{
     protected int numberBabies;
 
-    public Mammalia(Color color, Size size, int numberBabies) : base(color, size) {
+    public Mammalia(Color color, Size size, int numberBabies)
+        : base(color, size)
+    {
         this.numberBabies = numberBabies;
     }
 
-    public void Run() {
-        Console.WriteLine("Moving: Running on the ground.");
+    public void Run()
+    {
+        Console.WriteLine("Moving: Running on the ground");
     }
 }
 
-abstract class Aves : Animal {
-    public Aves(Color color, Size size) : base(color, size) {}
+abstract class Aves : Animal
+{
+    public Aves(Color color, Size size) : base(color, size) { }
 
-    public void Fly() {
-        Console.WriteLine("Moving: Flying in the sky.");
+    public void Fly()
+    {
+        Console.WriteLine("Moving: Flying in the sky");
     }
 }
 
-abstract class Osteichthyes : Animal {
-    public Osteichthyes(Color color, Size size) : base(color, size) {}
+abstract class Osteichthyes : Animal
+{
+    public Osteichthyes(Color color, Size size) : base(color, size) { }
 
-    public void Swimming() {
-        Console.WriteLine("Moving: Swimming in the water.");
+    public void Swim()
+    {
+        Console.WriteLine("Moving: Swimming in the sea");
     }
 }
 
-// --- 4. Concrete Classes ---
-class Dog : Mammalia {
+// ======================
+
+class Dog : Mammalia
+{
     protected bool fierce;
 
-    public Dog(Color color, Size size, int numberBabies, bool fierce) : base(color, size, numberBabies) {
+    public Dog(Color color, Size size, int numberBabies, bool fierce)
+        : base(color, size, numberBabies)
+    {
         this.fierce = fierce;
     }
 
-    public override void Sound() {
-        Console.WriteLine("Sound: Woof Woof!");
-    }
+    public override void Sound() => Console.WriteLine("Sound: Wolf wolf");
+    public override void Eat() => Console.WriteLine("Food: Bones");
 
-    public override void Eat() {
-        Console.WriteLine("Eat: Dog food and bones.");
-    }
+    public void Bark() => Console.WriteLine("Action: Barking loud!");
 
-    public void Bark() {
-        Console.WriteLine("Action: Barking loudly!");
-    }
-
-    public void Bite() {
-        Console.WriteLine("Fierce Status: " + fierce); 
-        Console.WriteLine("Action: Warning! It bites!");
+    public void Bite()
+    {
+        Console.WriteLine($"Fierce Status: {fierce}");
+        Console.WriteLine("It gonna bite you");
     }
 }
 
-class Bird : Aves {
-    protected string eggDesc;
+class Bird : Aves
+{
+    protected string egg;
 
-    public Bird(Color color, Size size, string eggDesc) : base(color, size) {
-        this.eggDesc = eggDesc;
+    public Bird(Color color, Size size, string egg) : base(color, size)
+    {
+        this.egg = egg;
     }
 
-    public override void Sound() {
-        Console.WriteLine("Sound: Chirp Chirp!");
-    }
-
-    public override void Eat() {
-        Console.WriteLine("Eat: Worms and seeds.");
-    }
+    public override void Sound() => Console.WriteLine("Sound: Chip chip");
+    public override void Eat() => Console.WriteLine("Food: Worms");
 }
 
-class Fish : Osteichthyes {
+class Fish : Osteichthyes
+{
     protected string waterGroup;
 
-    public Fish(Color color, Size size, string waterGroup) : base(color, size) {
+    public Fish(Color color, Size size, string waterGroup) : base(color, size)
+    {
         this.waterGroup = waterGroup;
     }
 
-    public override void Sound() {
-        Console.WriteLine("Sound: ... (Glub Glub)");
-    }
-
-    public override void Eat() {
-        Console.WriteLine("Eat: Plankton and small fish.");
-    }
+    public override void Sound() => Console.WriteLine("Sound: Pop pop");
+    public override void Eat() => Console.WriteLine("Food: Small Fish");
 }
 
-// --- 5. Specific Species ---
-class ThaiRidgeBack : Dog {
+// ======================
+
+class ThaiRidgeBack : Dog
+{
     private string origin;
 
-    public ThaiRidgeBack(Color color, Size size, int numberBabies, bool fierce, string origin) 
-        : base(color, size, numberBabies, fierce) {
-        this.origin = origin;
+    public ThaiRidgeBack(Color c, Size s, int n, bool f, string o)
+        : base(c, s, n, f)
+    {
+        origin = o;
     }
 
-    public void ShowOrigin() {
-        Console.WriteLine("Origin: " + origin);
-    }
+    public void ShowOrigin() => Console.WriteLine("Origin: " + origin);
 }
 
-class HummingBird : Bird {
+class HummingBird : Bird
+{
+    public HummingBird(Color c, Size s, string e) : base(c, s, e) { }
+}
+
+class AngleFish : Fish
+{
     private string location;
 
-    public HummingBird(Color color, Size size, string eggDesc, string location) 
-        : base(color, size, eggDesc) {
-        this.location = location;
+    public AngleFish(Color c, Size s, string w, string l) : base(c, s, w)
+    {
+        location = l;
     }
 
-    public void ShowLocation() {
-        Console.WriteLine("Location: " + location);
-    }
+    public void ShowLocation() => Console.WriteLine("Location: " + location);
 }
 
-class AngelFish : Fish {
-    public AngelFish(Color color, Size size, string waterGroup) : base(color, size, waterGroup) {}
-}
+// ======================
 
-// --- 6. Main Execution ---
-class Program {
-    static void Main(string[] args) {
-        ThaiRidgeBack trb = new ThaiRidgeBack(Color.BROWN, Size.MEDIUM, 4, true, "Thailand");
-        trb.PrintInfo();
-        trb.Eat();
-        trb.Run();
-        trb.Sound();
-        trb.Bark();
-        trb.Bite();
-        trb.ShowOrigin();
+class Program
+{
+    static void Main()
+    {
+        ThaiRidgeBack dog = new ThaiRidgeBack(Color.BROWN, Size.MEDIUM, 5, true, "Thailand");
+        dog.Info(); dog.Sound(); dog.Eat(); dog.Run(); dog.Bark(); dog.Bite(); dog.ShowOrigin();
 
-        Console.WriteLine("-------------------------------");
+        Console.WriteLine("----------------");
 
-        HummingBird hmb = new HummingBird(Color.GREY, Size.SMALL, "Tiny White Egg", "Tropical Forest");
-        hmb.PrintInfo();
-        hmb.Fly();
-        hmb.Eat();
-        hmb.ShowLocation();
+        HummingBird bird = new HummingBird(Color.WHITE, Size.SMALL, "Small egg");
+        bird.Info(); bird.Fly(); bird.Sound(); bird.Eat();
 
-        Console.WriteLine("-------------------------------");
+        Console.WriteLine("----------------");
 
-        AngelFish af = new AngelFish(Color.WHITE_STRIPES, Size.SMALL, "Saltwater");
-        af.PrintInfo();
-        af.Swimming();
-        af.Eat();
+        AngleFish fish = new AngleFish(Color.GREY, Size.SMALL, "Salt Water", "Pacific Ocean");
+        fish.Info(); fish.Swim(); fish.Sound(); fish.Eat(); fish.ShowLocation();
     }
 }
